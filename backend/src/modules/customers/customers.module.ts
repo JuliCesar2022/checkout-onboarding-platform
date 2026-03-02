c9a22aab2f7d6db406d761829e44269a1ed5c0bc
Julio Bonifacio <juliobonifacio53@gmail.com>
2026-03-01 22:15:11 -0500

    feat: add initial customers module with service and repository setup

--- backend/src/modules/customers/customers.module.ts
+++ backend/src/modules/customers/customers.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CustomersController } from './infrastructure/controllers/customers.controller';
 import { CustomersService } from './application/customers.service';
 import { ICustomersRepository } from './domain/repositories/customers.repository';
 import { PrismaCustomersRepository } from './infrastructure/repositories/prisma-customers.repository';
 
 @Module({
-  controllers: [CustomersController],
+  controllers: [],
   providers: [
     CustomersService,
     {
